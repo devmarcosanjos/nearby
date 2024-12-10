47de874a82733d131d1c409f7a93da5edb6afe73
Marcos Anjos <dev.marcosanjos@gmail.com>
2024-12-09 22:51:27 -0300

    feat: add layout, theme, and color styles; implement index with Welcome and Steps components

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { Steps } from "@/src/components/steps";
+import { Welcome } from "@/src/components/welcome";
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+      <Welcome />
+
+      <Steps />
+    </View>
+  );
+}
