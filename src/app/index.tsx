7e503697f3acf39cbbe994fb2726724fd3947324
Marcos Anjos <dev.marcosanjos@gmail.com>
2024-12-09 23:05:41 -0300

    feat: add react-native-svg dependency; update Step and Steps components to include icons

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,7 +6,6 @@ export default function Index() {
   return (
     <View style={{ flex: 1, padding: 40, gap: 40 }}>
       <Welcome />
-
       <Steps />
     </View>
   );
--- src/components/steps/index.tsx
+++ src/components/steps/index.tsx
@@ -1,20 +1,29 @@
 import { Text, View } from "react-native";
 import { Step } from "@/src/components/step";
 import { s } from "./style";
+import {
+  IconMapPin,
+  IconQrcode,
+  IconTicket,
+  IconTicTac,
+} from "@tabler/icons-react-native";
 
 export function Steps() {
   return (
     <View style={s.container}>
       <Text style={s.title}>Veja como funciona:</Text>
       <Step
+        icon={IconMapPin}
         title="Encontre estabelicimentos"
         description="Veja locais perto de você que são parceiros dos Nearby"
       />
       <Step
+        icon={IconQrcode}
         title="Ative o cupom com QR Code"
         description="VEscaneie o código QR no estabelecimento para ativar o cupom"
       />
       <Step
+        icon={IconTicket}
         title="Garanta as vantavens perto de você"
         description="Ative cupons onde estiver em diferentes tipos de estabelecimentos."
       />
