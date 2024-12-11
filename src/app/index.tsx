bf78a51fd72ba88d5aaa0a6e939e4c46793b8e1d
Marcos Anjos <dev.marcosanjos@gmail.com>
2024-12-11 08:02:47 -0300

    feat: add Home screen and API service; update index to navigate to Home and include axios dependency

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/src/components/button";
 import { Steps } from "@/src/components/steps";
 import { Welcome } from "@/src/components/welcome";
+import { router } from "expo-router";
 import { View } from "react-native";
 
 export default function Index() {
@@ -8,7 +9,11 @@ export default function Index() {
     <View style={{ flex: 1, padding: 40, gap: 40 }}>
       <Welcome />
       <Steps />
-      <Button>
+      <Button
+        onPress={() => {
+          router.navigate("/home");
+        }}
+      >
         <Button.Title>Começar</Button.Title>
       </Button>
     </View>
